6640e03a52be3b31f557d7957f6becfd324dd82f
travisjlewis <travis.lewis@students.olin.edu>
2013-09-30 16:54:01 -0400

    Initial Commit

---
+++ Bit_Structure.py
@@ -0,0 +1,45 @@
+Bit_structure(object):
+    
+    def __init__(self):
+        self.transmit_field_offset = 0
+        
+    def register(self,arg_Bit_field_type):
+
+        specified_bdf_offset, bdf_type = arg_bit_data_field
+
+        _offset = self.bit_field_offset
+
+        if predicted_bdf_offset != calculated_bdf_offset:
+            raise ValueError(
+                "Bit Data Field offset error:\n"+\
+                "Predicted %n, Calculated %n"%predicted_offset,calculated_offset)
+            
+        self.bit_field_offset  +=  bfd_type.length
+        self.bit_fields.append(
+            Bit_Data_Field(self.bit_field_type,length)
+
+            
+class Bit_field_type(object):
+    def __init__(self)      :raise NotImplemented
+    def __eq__  (self,other):raise NotImplemented
+    def __le__  (self,other):raise NotImplemented
+    def __len__ (self)      :raise NotImplemented
+    def __repr__(self)      :raise NotImplemented
+    def __inv__ (self)      :raise NotImplemented
+    def __or_   (self,other):raise NotImplemented
+    def __and__ (self,other):raise NotImplemented
+    def __xor__ (self,other):raise NotImplemented
+    def __add__ (self,other):raise NotImplemented
+    def __sub__ (self,other):raise NotImplemented
+    def __str__ (self)      :raise NotImplemented
+        
+class Bit_field_bit(Bit_field_type):
+    def __init__(self,identifier):
+        self.identifier = identifier
+            
+            
+class Bit_field_netascii(Bit_field_type):
+            
+            
+            
+        
